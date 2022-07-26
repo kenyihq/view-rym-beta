1a6b91f004e2a729602dffdc307e1e7b522a1b1f
Kenyi Hancco <kenyihq@gmail.com>
2022-07-26 00:24:49 -0500

    Added city list

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-const getDataTravel = () => {
-    let origin = document.getElementById('origin').value;
 
-    document.getElementById('printResult').innerHTML = origin;
+function getCity(){
+
+    let iata_peru = ['Arequipa', 'Andahuaylas', 'Anta (Huaraz)', 'Ayacucho', 'Cajamarca', 'Chimbote', 'Cuzco', 'Chachapoyas', 'Chiclayo', 'Huanuco', 'Ilo', 'Iquitos', 'Juanjui', 'Juliaca', 'Lima', 'Moyobamba', 'Pisco', 'Piura', 'Pucallpa', 'Puerto Maldonado', 'Rioja', 'Tacna', 'Talara', 'Tarapoto', 'Tingo Maria', 'Trujillo', 'Tumbes', 'Yurimaguas'];
+    
+    let options = '';
+    
+    for (let i = 0; i < iata_peru.length; i++){
+        options += `<option value="${iata_peru[i]}">${iata_peru[i]}</option>\n`;
+    }
+    // console.log(options);
+    document.getElementById('origin').innerHTML = options;
+    document.getElementById('destination').innerHTML = options;
 }
+    
+
+const getDataTravel = () => {
+    // let origin = document.getElementById('origin').value;
+    let origin = document.getElementById('get-origin').value;
+    let destination = document.getElementById('get-destination').value;
+    let date = document.getElementById('date').value;
+
+    let sale = `${origin}-${destination}-${date}`;
+    sale += '-';
+    sale += '\nSoy el tercer renglon';
+
+    document.getElementById('printResult').innerHTML = sale;
+}
+
+
+getCity();
 
