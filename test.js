a4d52191c606662e1ac2b7654d403106ff7d9f06
Kenyi Hancco <kenyihq@gmail.com>
2022-08-09 10:22:35 -0500

    Added two function additional

--- test.js
+++ test.js
@@ -1,55 +1,81 @@
 class Flight{
 
     printTitle(){
-        let printProposal = '*PROPUESTA* 📝\n';
-        let printFlight = '*-VUELO DE IDA  🛫*\n';
-        let printRoundTrip = '*-VUELO DE VUELTA  🛫*\n';
+        let printProposal = '*PROPUESTA* 📝\n\n';
+        let printFlight = '*-VUELO DE IDA  🛫*\n\n';
+        let printRoundTrip = '*-VUELO DE VUELTA  🛫*\n\n';
 
         let result = [printProposal, printFlight, printRoundTrip];
 
         return result;
     }
 
-    oneWay(origin, destination, hour, departure, arrival, price){
+    infoFlight(origin, destination, hour, date){
         this.typeTrip = 'One Way';
-        this.origin = origin;
-        this.destination = destination;
-        this.hour = hour;
-        this.departure = departure;
-        this.arrival = arrival;
-        this.price = price;
 
         let printProposal = `*_${origin} - ${destination}_*\n`;
-        printProposal += `*__*\n`
-        printProposal += `*_Hora de salida: _*\n\n`
+        printProposal += `*_${date}_*\n`;
+        printProposal += `*_Hora de salida: ${hour}_*\n\n`;
     
         return printProposal;
     }
 
-    roundTrips(oneWay, origin, destination, hour, departure, arrival, price){
-        this.typeTrip = 'Round Trip';
-        this.oneWay = oneWay;
-        this.origin = origin;
-        this.destination = destination;
-        this.hour = hour;
-        this.departure = departure;
-        this.arrival = arrival;
-        this.price = price;
-    
-        return `${this.typeTrip} - ${this.oneWay} - ${this.origin} - ${this.destination} - ${this.hour} - ${this.departure} - ${this.arrival} - ${this.price}`;
-    }
-
 }
 
-let flight = new Flight();
-let flightO = flight.oneWay('Arequipa', 'Lima', '12:00', '12:00', '12:00', '$100');
+let newFlight = new Flight();
 
-//console.log(flight);
-let rt = flight.roundTrips(flightO, 'Lima', '12:00', '12:00', '12:00', '$100');
+function newFlightOW(origin, destination, hour, date){
+    // let printNewFlight = newFlight.printTitle()[0];
+    // printNewFlight += newFlight.printTitle()[1];
+    let printNewFlight = newFlight.infoFlight(origin, destination, hour, date);
 
+    return printNewFlight;
+}
 
-let pt = flight.printTitle();
+function newFlightRW(origin, destination, hour, date){
 
-console.log(pt[0]);
-console.log(pt[1]);
-console.log(flightO);
+    let rhour = '02:30pm';
+    let rdate = 'Viernes 12 de Agosto';
+    
+    let printNewFlight = newFlightOW(origin, destination, hour, date);
+    printNewFlight += newFlight.printTitle()[2]; 
+    printNewFlight += newFlight.infoFlight(destination, origin, rhour, rdate);
+    
+    return printNewFlight;
+}
+
+const quote = 'One Way';
+const option = 'Onse Way';
+
+let newQuote = '';
+
+// Declare local variables
+let printT = newFlight.printTitle()[0];
+let printOW = newFlight.printTitle()[1];
+let printRT = newFlight.printTitle()[2];
+
+let origin = 'Cancun';
+let destination = 'Puerto Vallarta';
+let hour = '10:00am';
+let date = 'Viernes 12 de Agosto';
+
+if (quote == option) {
+    newQuote = printT;
+    newQuote += printOW;
+    newQuote += newFlight.infoFlight(origin, destination, hour, date);
+}
+else {
+
+    // Additional variables
+    let rhour = '03:30pm';
+    let rdate = 'Sábado 13 de Agosto';
+    
+    newQuote = printT;
+    newQuote += printOW;
+    newQuote += newFlight.infoFlight(origin, destination, hour, date);
+    newQuote += printRT;
+    newQuote += newFlight.infoFlight(destination, origin, rhour, rdate);
+}
+
+console.log(newQuote);
+
